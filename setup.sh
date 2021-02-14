ddf4331ce6ac6f59d281a8ddeadfe130ceb48e27
sundahlc <sundahlc@gmail.com>
2021-02-13 18:05:53 -0800

    reupload for Heroku

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 mkdir -p ~/.streamlit/
-echo "[general]
-email = \"sundahlc@gmail.com\"
+echo "\
+[general]\n\
+email = \"sundahlc@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "[server]
-headless = true
-enableCORS=false
-port = $PORT
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
 " > ~/.streamlit/config.toml
