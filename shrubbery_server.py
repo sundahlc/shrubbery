51a27f3df997442bcedc8fd5d25eb488a4b2bbab
sundahlc <sundahlc@gmail.com>
2021-03-13 21:33:11 -0800

    make player status in database

--- shrubbery_server.py
+++ shrubbery_server.py
@@ -1,227 +1,317 @@
-import streamlit as st
 import pandas as pd
+from datetime import datetime
 import psycopg2
 import urllib.parse
 import os
 
-from streamlit.report_thread import get_report_ctx
+import streamlit as st
+from streamlit.hashing import _CodeHasher
 
-class SessionState(object):
-    def __init__(self, **kwargs):
-        """A new SessionState object.
+try:
+    # Before Streamlit 0.65
+    from streamlit.ReportThread import get_report_ctx
+    from streamlit.server.Server import Server
+except ModuleNotFoundError:
+    # After Streamlit 0.65
+    from streamlit.report_thread import get_report_ctx
+    from streamlit.server.server import Server
 
-        Parameters
-        ----------
-        **kwargs : any
-            Default values for the session state.
+#======================================================================================================================
+# MAIN PAGE
+#======================================================================================================================
 
-        Example
-        -------
-        >>> session_state = SessionState(user_name='', favorite_color='black')
-        >>> session_state.user_name = 'Mary'
-        ''
-        >>> session_state.favorite_color
-        'black'
+os.chdir(r'C:\Users\Kuri y Rizu\Documents\Synced Folders\Programming\shrubbery\shrubbery')
 
-        """
-        for key, val in kwargs.items():
-            setattr(self, key, val)
+def main():
+    state = _get_state()
+
+    loader_name = st.sidebar.text_input('name')
+    # if st.sidebar.button('Load me up Scottie'):
+    load_player(state, loader_name)
+    # st.sidebar.write(repr(state.player.__dict__))
+
+    if state.player.active == True:
+        if st.checkbox('pssst', value=False):
+            active_player(state)
+
+    show_columns(state)
+
+    # st.sidebar.write(repr(state.__dict__))
+    if st.sidebar.button('Clear state'):
+        state.clear()
+
+    # Mandatory to avoid rollbacks with widgets, must be called at the end of your app
+    state.sync()
 
 
-@st.cache(allow_output_mutation=True)
-def get_session(id, **kwargs):
-    return SessionState(**kwargs)
+#======================================================================================================================
+# Database class
+#======================================================================================================================
 
 
-def get(**kwargs):
-    """Gets a SessionState object for the current session.
+class db_talker():
+    '''
+    Usage:
+    with db_talker() as cur:
+        cur.execute(...)
+    Automatically closes at the end of with block.
+    '''
 
-    Creates a new object if necessary.
+    def __enter__(self):
+        self.conn = self.connect_to_elephantsql()
+        self.cur = self.conn.cursor()
+        return self.cur
 
-    Parameters
-    ----------
-    **kwargs : any
-        Default values you want to add to the session state, if we're creating a
-        new one.
+    def __exit__(self, *args):
+        self.conn.commit()
+        self.conn.close()
 
-    Example
-    -------
-    >>> session_state = get(user_name='', favorite_color='black')
-    >>> session_state.user_name
-    ''
-    >>> session_state.user_name = 'Mary'
-    >>> session_state.favorite_color
-    'black'
+    def connect_to_elephantsql(self):
+        # Connect to ElephantSQL database
+        urllib.parse.uses_netloc.append("postgres")
+        try:
+            DATABASE_URL = os.environ['DATABASE_URL']
+        except KeyError:
+            DATABASE_URL = open(r'../shrubbery_login.txt', 'r').read().strip()
+        url = urllib.parse.urlparse(DATABASE_URL)
+        conn = psycopg2.connect(database=url.path[1:],
+        user = url.username,
+        password = url.password,
+        host = url.hostname,
+        port = url.port
+        )
 
-    Since you set user_name above, next time your script runs this will be the
-    result:
-    >>> session_state = get(user_name='', favorite_color='black')
-    >>> session_state.user_name
-    'Mary'
+        return conn
 
-    """
-    ctx = get_report_ctx()
-    id = ctx.session_id
-    return get_session(id, **kwargs)
+
+#======================================================================================================================
+# PLAYER definitions
+#======================================================================================================================
+
 
 class player():
-    def __init__(self):
+    def __init__(self, name):
+        self.name = name.lower()
         self.cards = {}
         self.story = ''
-        self.discard = []
-        self.checkedout_ids = []
-        self.points = 20
+        self.points = 5
+        self.active = False
+
+        with db_talker() as cur:
+            cur.execute(f'''insert into players (name, active)
+                            values ('{self.name}', false)
+                            on conflict do nothing''')
+            cur.execute(f'''select id from players where name = '{self.name}' ''')
+            self.player_id = cur.fetchone()[0]
+
+            cur.execute(f'''select points from players where id={self.player_id}''')
+            self.points = cur.fetchone()[0]
 
     def reset(self):
         self.cards = {}
+        self.selected = {}
         self.story = ''
-        self.discard = []
-        self.checkedout_ids = []
 
-# @st.cache(allow_output_mutation=True)
-# def load_player():
-#     player_1 = player()
-#     return player_1
 
-session_state = get(session_player=player())
 
-def connect_to_elephantsql():
-    # Connect to ElephantSQL database
-    urllib.parse.uses_netloc.append("postgres")
-    try:
-        DATABASE_URL = os.environ['DATABASE_URL']
-    except:
-        DATABASE_URL = open(r'../shrubbery_login.txt', 'r').read().strip()
-    url = urllib.parse.urlparse(DATABASE_URL)
-    conn = psycopg2.connect(database=url.path[1:],
-    user = url.username,
-    password = url.password,
-    host = url.hostname,
-    port = url.port
-    )
+#======================================================================================================================
+# Button Definitions
+#======================================================================================================================
 
-    return conn
 
-def hit_me(conn):
-    cur = conn.cursor()
-    cur.execute("select id, contents, type from cards where type = 'word' and checkedout = False order by random() limit 1")
-    id, contents, type = cur.fetchone()
-    cur.execute(f"update cards set checkedout = True where id = {id}")
-    conn.commit()
-    cur.close()
-    return id, contents, type
+def load_player(state, name):
+    name = name.lower()
+    if name not in ('chris', 'mike', 'nick', 'christian'):
+        st.sidebar.write("You're not really in this game!")
+        st.stop()
+    state.player = player(name)
+    with db_talker() as cur:
+        cur.execute(f'''select cards.id, contents, type 
+                        from players join cards
+                        on players.id = cards.status
+                        where players.id = {state.player.player_id}''')
+        raw_hand_cards = cur.fetchall()
 
-def story_time(conn):
-    cur = conn.cursor()
-    cur.execute("select id, contents from cards where type = 'story' and checkedout = False order by random() limit 1")
-    id, contents = cur.fetchone()
-    cur.execute(f"update cards set checkedout = True where id = {id}")
-    conn.commit()
-    cur.close()
-    return id, contents
+        cur.execute(f'''select active from players where id = {state.player.player_id}''')
+        state.player.active = cur.fetchone()[0]
 
-def modifier(conn):
-    cur = conn.cursor()
-    cur.execute('''select id, type, contents from cards where type = 'modifier' or type = 'foreshadow' 
-                    and checkedout = False order by random() limit 1''')
-    id, card_type, contents = cur.fetchone()
-    cur.execute(f"update cards set checkedout = True where id = {id}")
-    conn.commit()
-    cur.close()
-    return id, card_type, contents
+    for card in raw_hand_cards:
+        id, contents, type = card
+        state.player.cards[id] = type + ' | ' + contents
 
-def get_all_modifiers(conn):
-    query = "SELECT contents, type FROM CARDS WHERE TYPE='modifier' OR TYPE='foreshadow' ORDER BY TYPE"
-    modifiers = pd.read_sql(query, conn)
-    return modifiers
 
-conn = connect_to_elephantsql()
+def draw_card(state, deck):
+    with db_talker() as cur:
+        cur.execute(f'''select id, contents, type from cards where deck = '{deck}' and status is NULL 
+                        order by random() limit 1''')
+        card_id, contents, type = cur.fetchone()
+        cur.execute(f"update cards set status = {state.player.player_id} where id = {card_id}")
 
-# player = load_player()
+        state.player.points = state.player.points - 1
+        cur.execute(f'''update players set points={state.player.points} where id={state.player.player_id}''')
+        # cur.execute(f"insert into hands (player_id, card_id) values ({state.player.player_id}, {card_id}")
 
-if st.sidebar.button('Shuffle my cards back'):
-    cur = conn.cursor()
-    sql = 'update cards set checkedout = False where '
-    for card_id in session_state.session_player.checkedout_ids:
-        sql = sql + f'id = {card_id} or '
-    sql = sql[:len(sql)-4]
-    cur.execute(sql) # remember to add functionality to only shuffle your cards back in
-    conn.commit()
-    cur.close()
-    session_state.session_player.reset()
-    
-column_1, column_2 = st.beta_columns([1,2])
-story_spot = column_2.empty()
+    state.player.cards[card_id] = type + ' | ' + contents
 
-if column_1.button('Hit me with them words'):
-    try:
-        card_id, card_content, card_type = hit_me(conn)
-        session_state.session_player.cards[f'{card_type} | {card_content}'] = False
-        session_state.session_player.checkedout_ids.append(card_id)
-        session_state.session_player.points = session_state.session_player.points - 1
-    except:
-        st.header('YOU RUN OUTTA CAHDS MATE')
 
-if column_1.button('Gimme a modifier'):
-    try:
-        card_id, card_type, card_content = modifier(conn)
-        session_state.session_player.cards[f'{card_type} | {card_content}'] = False
-        session_state.session_player.checkedout_ids.append(card_id)
-        session_state.session_player.points = session_state.session_player.points - 2
-    except:
-        st.header("ah man we're all out of modifiers")
+def act_on_card(state, action, card_id):
+    choices = {'discard':-1, 'send':0}
+    state.player.cards.pop(card_id)
+    with db_talker() as cur:
+        cur.execute(f'''update cards set status={choices[action]} where id={card_id}''')
+        # cur.execute(f'''delete from hands where player_id={state.player.player_id} and card_id={card_id}''')
 
-if column_1.button('Story time'):
-    try:
-        card_id, card_content = story_time(conn)
-        session_state.session_player.story = card_content
-        session_state.session_player.checkedout_ids.append(card_id)
-    except:
-        st.header('YOU RUN OUTTA STORY CAHDS MATE')
 
-if column_1.button('Burn this'):
-    for card, ditch in session_state.session_player.cards.copy().items():
-        if ditch:
-            session_state.session_player.discard.append(card)
-            _ = session_state.session_player.cards.pop(card)
+#======================================================================================================================
+# Display flow
+#======================================================================================================================
 
-for card in session_state.session_player.cards.keys():
-    session_state.session_player.cards[card] = column_2.checkbox(card, key=card)
-story_spot.markdown(f'### {session_state.session_player.story}')
+def active_player(state):
+    st.markdown(f"## HEY {state.player.name.upper()}! You're the active player!")
+    with db_talker() as cur:
+        cur.execute('select type, contents from cards where status=0')
+        active_cards = cur.fetchall()
+    st.write(f"You have {len(active_cards)} cards sent ... so far.")
+    st.button('Refresh! Do I have more cards?')
+    if st.button('See your cards & WRITE (timer starts immediately!)'):
+        state.turn = 'writing'
+        with db_talker() as cur:
+            cur.execute('update turn set accepting=false')
+            cur.execute(f'update turn set time={datetime.timestamp(datetime.now())}')
+    if state.turn == 'writing':
+        st.success('GO!')
+        for card in active_cards:
+            type, contents = card
+            st.write(type + ' | ' + contents)
 
-session_state.session_player.points = column_2.number_input('points', min_value=0, max_value=None,
-                                                            value=session_state.session_player.points)
+        if st.button('stop writing'):
+            state.turn = 'judging'
+            end_writing(state)
+    if state.turn == 'judging':
+        minutes = round(state.writing_time // 60)
+        seconds = round(state.writing_time % 60)
+        st.write(f'You wrote for {minutes}:{seconds}.')
 
-# player_name = st.sidebar.text_input(label='name')
-# if st.sidebar.button("I'm Mike"):
-#     if player_name.lower() == 'mike':
-#         st.table(get_all_modifiers(conn))
 
-# st.sidebar.write(repr(player.__dict__))
 
-conn.close()
+def end_writing(state):
+    with db_talker() as cur:
+        cur.execute('update turn set accepting=true')
+        cur.execute('select time from turn')
+        t1 = cur.fetchone()[0]
+        cur.execute('update cards set status=-1 where status=0')
+        cur.execute('update players set active=false')
 
-st.sidebar.markdown('### Rules reminder')
-rules = '''
-Word card = 1 point
-Modifier card = 2 points
+    t2 = datetime.timestamp(datetime.now())
+    state.writing_time = t2 - t1
 
-### Judging:
-Integrating your own cards: 
-- 2 points each
 
-Someone else integrates your cards:
-- 2 points for a word,
-- 3 points for a modifier
+def show_columns(state):
+    column_1, column_2 = st.beta_columns([1,2])
 
-### Creating elements:
-- 7 points for a character or other element
-- 5 points for a trait
-- 5 points to create your own modifier card
-- 5 points to create your own story card
-- 5 points to create your own foreshadow card
-- 5 points to create your own question card
-- Doesn't include the points you load onto it!
-'''
+    # Left Column
+    point_display = column_1.empty()
+    deck_buttons = {'Story time':'story', 'Give me a normal card':'normal', 'How about a special card':'special'}
+    for text, deck in deck_buttons.items():
+        if column_1.button(text):
+            draw_card(state, deck)
 
-st.sidebar.markdown(rules)
+    for action in ('Discard', 'Send'):
+        if st.button(action):
+            for card_id, bool in state.selection.items():
+                if bool == True:
+                    act_on_card(state, action.lower(), card_id)
+
+    point_display.number_input('Points', state.player.points, step=1)
+
+
+    # Right Column
+    state.selection = dict()
+    for card_id, contents in state.player.cards.items():
+        state.selection[card_id] = column_2.checkbox(contents, value=False)
+
+
+
+#======================================================================================================================
+# Session State
+#======================================================================================================================
+
+
+class _SessionState:
+
+    def __init__(self, session, hash_funcs):
+        """Initialize SessionState instance."""
+        self.__dict__["_state"] = {
+            "data": {},
+            "hash": None,
+            "hasher": _CodeHasher(hash_funcs),
+            "is_rerun": False,
+            "session": session,
+        }
+
+    def __call__(self, **kwargs):
+        """Initialize state data once."""
+        for item, value in kwargs.items():
+            if item not in self._state["data"]:
+                self._state["data"][item] = value
+
+    def __getitem__(self, item):
+        """Return a saved state value, None if item is undefined."""
+        return self._state["data"].get(item, None)
+
+    def __getattr__(self, item):
+        """Return a saved state value, None if item is undefined."""
+        return self._state["data"].get(item, None)
+
+    def __setitem__(self, item, value):
+        """Set state value."""
+        self._state["data"][item] = value
+
+    def __setattr__(self, item, value):
+        """Set state value."""
+        self._state["data"][item] = value
+
+    def clear(self):
+        """Clear session state and request a rerun."""
+        self._state["data"].clear()
+        self._state["session"].request_rerun()
+
+    def sync(self):
+        """Rerun the app with all state values up to date from the beginning to fix rollbacks."""
+
+        # Ensure to rerun only once to avoid infinite loops
+        # caused by a constantly changing state value at each run.
+        #
+        # Example: state.value += 1
+        if self._state["is_rerun"]:
+            self._state["is_rerun"] = False
+
+        elif self._state["hash"] is not None:
+            if self._state["hash"] != self._state["hasher"].to_bytes(self._state["data"], None):
+                self._state["is_rerun"] = True
+                self._state["session"].request_rerun()
+
+        self._state["hash"] = self._state["hasher"].to_bytes(self._state["data"], None)
+
+
+def _get_session():
+    session_id = get_report_ctx().session_id
+    session_info = Server.get_current()._get_session_info(session_id)
+
+    if session_info is None:
+        raise RuntimeError("Couldn't get your Streamlit Session object.")
+
+    return session_info.session
+
+
+def _get_state(hash_funcs=None):
+    session = _get_session()
+
+    if not hasattr(session, "_custom_session_state"):
+        session._custom_session_state = _SessionState(session, hash_funcs)
+
+    return session._custom_session_state
+
+
+if __name__ == "__main__":
+    main()
